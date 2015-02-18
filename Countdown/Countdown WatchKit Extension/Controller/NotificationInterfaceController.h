f92a1a383f49f0aa230d317ed8d964e4811f8e68
Rostyslav Stepanyak <ros.aphex@gmail.com>
2015-02-18 12:10:05 +0200

    fix option screen layout

---
+++ Countdown/Countdown WatchKit Extension/Controller/NotificationInterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationInterfaceController.h
+//  Countdown
+//
+//  Created by Ross on 2/18/15.
+//  Copyright (c) 2015 Umbrella. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationInterfaceController : WKUserNotificationInterfaceController
+
+@end
---
+++ Countdown/Countdown WatchKit Extension/Controller/NotificationInterfaceController.m
@@ -0,0 +1,38 @@
+//
+//  NotificationInterfaceController.m
+//  Countdown
+//
+//  Created by Ross on 2/18/15.
+//  Copyright (c) 2015 Umbrella. All rights reserved.
+//
+
+#import "NotificationInterfaceController.h"
+
+
+@interface NotificationInterfaceController()
+
+@end
+
+
+@implementation NotificationInterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+    
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
